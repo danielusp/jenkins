82b74974ff81163247a0f17b010bba9b1da24827
Daniel Mendonça <dmsantos@uolinc.com>
2021-03-01 13:20:00 -0300

    BUGFIX: Run jenkins

--- Makefile
+++ Makefile
@@ -13,15 +13,7 @@ install:
 	jenkins:dev
 
 run:
-	docker run --rm -d \
-	--name jenkins-dev \
-	-p 8080:8080 \
-	-p 50000:50000 \
-	-v "${PWD}/jenkins_home:/var/jenkins_home" \
-	-v "${PWD}/output:/var/output" \
-	-v "/run/docker.sock:/run/docker.sock" \
-	-v "/usr/bin/docker:/usr/bin/docker" \
-	jenkins:dev
+	./run.sh
 
 stop:
 	docker stop jenkins-dev
--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-(cd ~/nodejs/temp/jenkins/ && make run)
+docker run --rm -d \
+	--name jenkins-dev \
+	-p 8080:8080 \
+	-p 50000:50000 \
+	-v "${PWD}/jenkins_home:/var/jenkins_home" \
+	-v "${PWD}/output:/var/output" \
+	-v "/run/docker.sock:/run/docker.sock" \
+	-v "/usr/bin/docker:/usr/bin/docker" \
+	jenkins:dev
+
+sudo chmod 666 /var/run/docker.sock
 
 sleep 5
 google-chrome http://localhost:8080
